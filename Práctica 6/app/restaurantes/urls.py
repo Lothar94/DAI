c4af34bc657f642ede89802f16981ce31bacaf97
Lothar Soto Palma <lsp.lot94@gmail.com>
2017-12-16 17:44:43 +0100

    práctica 8 y 9 acabadas

--- Práctica 6/app/restaurantes/urls.py
+++ Práctica 6/app/restaurantes/urls.py
@@ -8,5 +8,9 @@ urlpatterns = [
   url(r'^create/$', views.create_restaurant, name='create_restaurant'),
   url(r'^edit/$', views.edit_restaurant, name='edit_restaurant'),
   url(r'^find/$', views.find_restaurant_view, name='find_restaurant_view'),
-  url(r'^find/(cuisine|name|borough|zip)/([\w]+)', views.find_restaurant, name='find_restaurant'),
+  url(r'^find/(cuisine|name|borough|zip)', views.find_restaurant, name='find_restaurant'),
+  url(r'^find/detail/(?P<id>[0-9]+)', views.restaurant_detail_view, name='detail_restaurant'),
+  url(r'^stadistics/$', views.stadistics_view, name='stadistics'),
+  url(r'^stadistics/get/$', views.stadistics, name='stadistics_ajax'),
+  url(r'^password/$', views.change_password, name='change_password'),
 ]
--- Práctica 6/app/restaurantes/views.py
+++ Práctica 6/app/restaurantes/views.py
@@ -1,8 +1,12 @@
 # restaurantes/views.py
-
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render, HttpResponse, redirect
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth.decorators import login_required
 
 from .forms import *
 from .models import restaurants
@@ -25,14 +29,24 @@ def test_template(request):
     }
     return render(request, 'test.html', context)
 
-def find_restaurant(request, type, data):
+@csrf_exempt
+def find_restaurant(request, type):
     if type == "cuisine":
+        data = request.POST.get('cuisine', '')
+        print(data)
         cursor = restaurants.find({"cuisine": data})
     elif type == "name":
+        data = request.POST.get('name', '')
+        print(data)
         cursor = restaurants.find({"name": data})
     elif type == "borough":
+        data = request.POST.get('borough', '')
+        print(data)
         cursor = restaurants.find({"borough": data})
     elif type == "zip":
+        data = request.POST.get('zip', '')
+        print(data)
+
         cursor = restaurants.find({"address.zipcode": data})
 
     paginator = Paginator(cursor, 10) # Show 25 contacts per page
@@ -43,7 +57,7 @@ def find_restaurant(request, type, data):
 
     ret = []
     for doc in paginator.page(page):
-        ret.append({"name": doc["name"], "cuisine": doc["cuisine"], "street": doc["address"]["street"], "number": doc["address"]["building"], "zip": doc["address"]["zipcode"], "borough": doc["borough"] })
+        ret.append({"id": doc["restaurant_id"], "name": doc["name"], "cuisine": doc["cuisine"], "street": doc["address"]["street"], "number": doc["address"]["building"], "zip": doc["address"]["zipcode"], "borough": doc["borough"] })
 
     return HttpResponse(json.dumps(ret), content_type="application/json")
 
@@ -55,6 +69,19 @@ def find_restaurant_view(request):
     }
     return render(request, 'forms/find_restaurant.html', context)
 
+def restaurant_detail_view(request, id):
+    cursor = restaurants.find({"restaurant_id": id})
+    final_doc = None
+
+    for doc in cursor:
+        final_doc = doc
+
+    context = {
+        "restaurant": final_doc,
+    }
+    return render(request, 'detail.html', context)
+
+@login_required
 def edit_restaurant(request):
     cursor = list(restaurants.find())
     iterator = [(i, cursor[i]) for i in range(0, len(cursor))]
@@ -85,6 +112,7 @@ def edit_restaurant(request):
     }
     return render(request, 'forms/edit_restaurant.html', context)
 
+@login_required
 def create_restaurant(request):
     if request.method == "POST":
             form = RestaurantForm(request.POST)
@@ -114,3 +142,37 @@ def create_restaurant(request):
         "form": form,
     }
     return render(request, 'forms/create_restaurant.html', context)
+
+@login_required
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Important!
+            messages.success(request, 'Your password was successfully updated!')
+            return render(request, 'registration/complete_change_password.html')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'registration/change_password.html', {
+        'form': form
+    })
+
+def stadistics(request):
+    cursor = restaurants.find({}, {'cuisine': 1})
+    total = cursor.count()
+    types = {}
+
+    for doc in cursor:
+        if doc["cuisine"] not in list(types.keys()):
+            types[doc["cuisine"]] = 1
+        else:
+            types[doc["cuisine"]] += 1
+
+    return HttpResponse(json.dumps(types), content_type="application/json")
+
+
+def stadistics_view(request):
+    return render(request, 'stadistics.html')
